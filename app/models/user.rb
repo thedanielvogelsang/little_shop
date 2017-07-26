c7d484a57c0f2a59c0e4baed8ff8e883001a898e
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-07-25 18:21:57 -0600

    user and course model tests testing validations and associations, tests still need a bug fix then they're ready; also created a features test for courses index... satisfies Story2 requirements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates_presence_of :email,
+                        :password,
+                        :first_name,
+                        :last_name
+  validates :username, presence: true, uniqueness: true
+  has_many :user_courses
+  has_many :courses, through: :user_courses
+  enum role: %w(default admin)
 end
