1a2aa22eee0a59cc6551312c723792fe944ed447
Yohanan Assefa <nanahoy@gmail.com>
2017-07-27 12:04:14 -0600

    adds new update route for decreasing course quantity in cart, creates feature test, tests pass except image issue on user_adds_courses_to_cart_spec. Updated add_path to post cart_path

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   post '/carts', to: "carts#create"
   get '/cart', to: "carts#show"
   delete '/cart', to: "carts#destroy"
-  post '/add', to: 'carts#add'
+  post '/cart', to: 'carts#update'
 end
