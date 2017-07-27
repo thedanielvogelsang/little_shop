6dcd7d20037db1b65488b990b895941af5e8ec63
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-07-27 17:43:50 -0600

    fixes users#show from :course_id to :user_id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
+    byebug
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(params[:course_id])
+    @user = User.find(params[:user_id])
+    # if @user == current_user
+    #   render :show
+    # else
+    #   render file: "public/404"
+    # end
   end
 end
