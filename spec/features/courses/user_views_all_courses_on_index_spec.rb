a243bc1bdad5614c13054793fc732550416145c5
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-29 15:02:35 -0600

    takes out cart session from courses controller

--- spec/features/courses/user_views_all_courses_on_index_spec.rb
+++ spec/features/courses/user_views_all_courses_on_index_spec.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'rails_helper'
 
 RSpec.feature "user goes to course index" do
@@ -20,20 +21,21 @@ RSpec.feature "user goes to course index" do
   end
 
   scenario "and cannot see retired courses" do
-    category = Category.create(title: "CategoryTitle")
-    course = Course.create(title: "Mathematics", price: 1245.00, description: "Advanced Computations", image: "http://image_source.com", category_id: category.id)
-    course2 = Course.create(title: "Statistics", price: 1245.00, description: "Advanced Stats", image: "http://image_source.com", category_id: category.id)
-    course3 = Course.create(title: "General Chemistry", price: 1245.00, description: "Physical Chem 101", image: "http://image_source.com", category_id: category.id)
-    course4 = Course.create(title: "Mathematics2.0", price: 1245.00, description: "Advanced Maths", image: "http://image_source.com", category_id: category.id)
+    category = create(:category)
+    timestamp = DateTime.now
+    course = create(:course, category: category)
+    course2 = create(:course, category: category)
+    course3 = create(:course, category: category, retired: timestamp)
+    course4 = create(:course, category: category, retired: timestamp)
     visit root_path
 
     expect(page).to have_content(course.title)
     expect(page).to have_content(course2.title)
-    expect(page).to have_content(course3.title)
-    expect(page).to have_content(course4.title)
+    expect(page).to_not have_content(course3.title)
+    expect(page).to_not have_content(course4.title)
     expect(page).to have_content(course.description)
     expect(page).to have_content(course2.description)
-    expect(page).to have_content(course3.description)
-    expect(page).to have_content(course4.description)
+    expect(page).to_not have_content(course3.description)
+    expect(page).to_not have_content(course4.description)
   end
 end
