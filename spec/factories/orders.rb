7eaab2d6e5c74e7eadb0dc0c7e74c44c9fc95e89
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-30 19:06:59 -0600

    migrate course orders

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,11 +1,6 @@
 FactoryGirl.define do
   factory :order do
-<<<<<<< HEAD
     user
     status 1
-=======
-    status 1
-    user
->>>>>>> remotes/origin/ya-story-13-viewing-past-orders
   end
 end
