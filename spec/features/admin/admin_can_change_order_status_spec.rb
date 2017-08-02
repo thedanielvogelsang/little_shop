cf3053120e1ea4c1ef4dbced10a3003d6f858420
Samantha Gongora <samantha.gongora@gmail.com>
2017-08-01 20:12:27 -0600

    adds tests for changing order status of ordered and paid orders

--- spec/features/admin/admin_can_change_order_status_spec.rb
+++ spec/features/admin/admin_can_change_order_status_spec.rb
@@ -8,25 +8,13 @@ RSpec.feature 'admin visits dashboard' do
 
   scenario "can change order status" do
     ordered_order = create(:order, status: 0)
-    paid_prder = create(:order, status: 1)
-    cancelled_order = create(:order, status: 2)
-    completed_order = create(:order, status: 3)
+    paid_order = create(:order, status: 1)
 
     visit 'admin/dashboard'
-
     click_button 'mark as paid'
-    expect(page).to have_link(ordered_order.id)
+    click_button 'mark as completed'
 
-    click_button 'paid'
-    expect(page).to have_link(paid_order.id)
-
-    click_button 'cancelled'
-    expect(page).to have_link(cancelled_order.id)
-
-    click_button 'completed'
-    expect(page).to have_link(completed_order.id)
-    expect(page).to_not have_link(paid_order.id)
-    expect(page).to_not have_link(cancelled_order.id)
-    expect(page).to_not have_link(ordered_order.id)
+    expect(ordered_order.status).to eq(1)
+    expect(paid_order.status).to eq(3)
   end
 end
