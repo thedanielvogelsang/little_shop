005f9cae224f7b7a68ccc9e41bfdd8fbe3841b0d
Desislava Todorova <dslavatodorova@gmail.com>
2017-08-02 19:05:18 -0600

    working to resolve orders feature spec bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find(params[:id])
+    @order = Order.find(session[:order_id])
     @courses = @order.courses
   end
 
