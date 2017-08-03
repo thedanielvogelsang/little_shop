487d3e3a7d858e1b49012056914ce15d4985ee5d
Daniel Vogelsang <dr.vogelsang26@gmail.com>
2017-08-03 08:35:27 -0600

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  validates :title, presence: true #, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true,  numericality: { greater_than_or_equal_to: 1 }
 
@@ -9,16 +9,10 @@ class Course < ApplicationRecord
   has_many :orders, through: :course_orders
 
   enum status: %w[active inactive]
-
-  def self.search(search)
-    where("lower(title) LIKE lower(?)", "%#{search.strip}%")
-  end
-
+  
   def self.search(search_term)
     if search_term
       where('name LIKE ?', "%#{search_term}%")
-      # where('name LIKE ?', "%#{search_term}%").limit(10)
-      # where('name LIKE ?', "%#{search_term}%").order('id DESC')
     else
       order('id DESC')
     end
