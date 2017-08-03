c987079329d7d185398336b5d75fe106b514193b
Yohanan Assefa <nanahoy@gmail.com>
2017-08-02 19:33:30 -0600

    local changes to seed file reverted back to how development was originally

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true #, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true,  numericality: { greater_than_or_equal_to: 1 }
 
